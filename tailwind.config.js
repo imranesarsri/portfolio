f37943e17bcd328daf9129ca51635b54e19abc3b
imrane sarsri <imransarsi.ii@gmail.com>
2024-02-19 00:12:08 +0100

    save

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ["./docs/**/*.{html,js}"],
 
   theme: {
+    colors: {
+      Light: {
+        'default': '#FFFFFF',
+        'primary': '#DDE4FD',
+        'secondary': '#B8C7FC',
+        'accent1': '#A9BBFC',
+        'accent2': '#86A0FC',
+      },
+      Dark: {
+        'default': '#020821',
+        'primary': '#0B1336',
+        'secondary': '#162254',
+        'accent1': '#3E4D87',
+        'accent2': '#7B88BA',
+      },
+    },
     extend: {},
   },
   plugins: [],
